de341b47676e97d5ffd05c913a2f4c4cc95586c4
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-03-03 15:35:53 -0300

    add components to next-app

--- apps/next-app/src/pages/_app.tsx
+++ apps/next-app/src/pages/_app.tsx
@@ -1,5 +1,22 @@
 import type { AppProps } from 'next/app'
 
+import { useEffect } from 'react';
+
+
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const {
+      applyPolyfills,
+      defineCustomElements,
+    } = require('@stencil-ui/core/loader');
+
+    applyPolyfills().then(() => {
+      defineCustomElements(window);
+    });
+
+    return () => {};
+  }, []);
+
   return <Component {...pageProps} />
 }
--- apps/next-app/src/pages/index.tsx
+++ apps/next-app/src/pages/index.tsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
+
+import { MyTextInputCustomEvent } from '@stencil-ui/core'
+import { MyHeading, MyText, MyTextInput } from '@stencil-ui/react'
+
 export default function Home() {
+  const [text, setText] = useState('');
+
+  function handleChangeText({ detail: value }: MyTextInputCustomEvent<string>) {
+    setText(value)
+  }
+
   return (
     <>
-      <h1>Hello world</h1>
+      <MyHeading text="Heading Component" />
+
+      <MyText text="Text Component" />
+
+      <MyTextInput value={text} onChangeInput={handleChangeText} />
+
+      <MyText text={text} />
     </>
   )
 }
