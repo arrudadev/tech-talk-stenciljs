6e3558d258876d9c510d567bdb2b701da05284e8
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2023-03-03 18:06:21 -0300

    setup add @stencil-ui/vue in vue app

--- apps/vue-app/src/main.js
+++ apps/vue-app/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { applyPolyfills, defineCustomElements } from '@stencil-ui/core/loader';
+
 createApp(App).mount('#app')
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
